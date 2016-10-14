646136e0548e3a11d76b9a0b8ee46002259872a9
LanceJernigan <lance@knoxweb.com>
2016-10-14 16:00:44 -0400

    Working API

---
+++ src/cloneState.js
@@ -0,0 +1,17 @@
+export const cloneState = val => {
+  
+  const type = typeof val
+  
+  const lookup = {
+    
+    object: v => ({...v}),
+    
+    array: v => ([...v]),
+    
+    default: v => v.copy()
+    
+  }
+  
+  return lookup.hasOwnProperty(type) ? lookup[type](val) : lookup.default()
+  
+}
--- src/index.js
+++ src/index.js
@@ -1,48 +1,104 @@
-const timeMachine = (preloadedState) => {
+import {cloneState} from './cloneState'
+
+export const timeMachine = (preloadedState) => {
   
-  let state = preloadedState ? [preloadedState] : []
-  let index = 0
-  
-  const updateIndex = (val = index) => {
-    
-    if (isNaN(val)) {
-      
-      val = index
-      
+  let tree = {
+    master: {
+      index: 0,
+      state: preloadedState ? [preloadedState] : []
     }
+  }
+  let currentBranchKey = 'master'
+  let currentBranch = tree.master
+  
+  /*
+   *  getState() - Returns the curent state of the Time Machine
+   *
+   *  @params - branchKey (string) - Specific branch to get current state
+   *          - branchIndex (int) - Specific index to get from branch state
+   *
+   *  @returns - State object
+   */
+  
+  const getState = (branchKey = currentBranchKey, branchIndex) => {
     
-    val = val > (state.length - 1) ? (state.length - 1) : val < 0 ? 0 : val
+    branchIndex = ! isNaN(branchKey) ? branchKey : branchIndex
+    branchKey = ! isNaN(branchKey) ? currentBranchKey : branchKey
     
-    index = val
+    const branch = getBranch(branchKey)
+    
+    return branch.state.length ? cloneState(branch.state[branch.index]) : 'No state.'
+    
+  }
+  
+  /*
+   *  getBranch - Get a branch by branchKey or current branch if no branchKey is supplied
+   *
+   *  @params - branchKey (string) - key of the branch you would like to retrieve
+   *
+   *  @returns - Branch object
+   */
+  
+  const getBranch = (branchKey = currentBranchKey) => {
+    
+    return tree.hasOwnProperty(branchKey) ? branchKey === currentBranchKey ? currentBranch : tree[branchKey] : 'No Branch.'
+    
+  }
+  
+  /*
+   *  next - Either get the next state of the current branch or define the next step of state in the branch
+   *
+   *  @params - val (any) - The next value of the current branch
+   */
+  
+  const next = val => {
+    
+    return val ? updateBranch({state: val}) : updateBranch({index: getBranch().index + 1})
+    
+  }
+  
+  /*
+   *  previous - Get the previous state of current branch
+   */
+  
+  const previous = () => {
+
+    return updateBranch({index: (getBranch().index - 1)})
+    
+  }
+  
+  /*
+   *  updateBranch - Updates the current branch with the given values
+   *
+   *  @params - branch (branch object) - The next step of the branch
+   */
+  
+  const updateBranch = ({index, state}) => {
+    
+    currentBranch = getBranch()
+    
+    currentBranch.index = state ? (currentBranch.state.push(state) - 1) : index > (currentBranch.state.length - 1) ? (currentBranch.state.length - 1) : index < 0 ? 0 : index
+    
+    tree[currentBranchKey] = currentBranch
     
     return getState()
     
   }
-  
-  const getState = () => state[index]
-  
-  const next = val => {
-    
-    state = [...state.slice(0, index + 1), val]
-    
-    return updateIndex(val ? (state.length - 1) : index + 1)
-    
-  }
-  
-  const previous = () => updateIndex(index - 1)
-  
-  const current = () => getState(state.length - 1)
-  
-  return {getState, next, previous, current}
+
+  return {getState, next, previous}
   
 }
 
-const test = timeMachine()
+/*
+ *  Example - Just a quick example of how to interact with the API
+ */
 
+const tree = timeMachine({test: 1})
 
-console.log(test.next({test: 1}))
-console.log(test.next({test: 2}))
-console.log(test.next({test: 3}))
-console.log(test.previous())
-console.log(test.previous())
-console.log(test.next({test: 4}))
+tree.next({test: 2})
+
+const prev = tree.previous()
+const next = tree.next()
+const curr = tree.getState()
+
+console.log(prev, next, curr)
